f8b91365116f487630e7c33de7acd68c08407f55
daniguillen <72428160+daniguillen@users.noreply.github.com>
2024-04-22 23:55:43 -0300

    reestablece busquedad query
    la conexion se reestablecio

--- Acciones/conexion.cs
+++ Acciones/conexion.cs
@@ -77,5 +77,70 @@ namespace Acciones
         }
 
 
+
+        public List<Articulo> Busquedad(string buscar)
+        {
+            List<Articulo> Lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand commando = new SqlCommand();
+            SqlDataReader lector;
+            try
+
+            {   //Distintas bases de conexiones
+                // conexion.ConnectionString = "server=PCFEDEX\\SQLEXPRESS; database=CATALOGO_P3_DB;integrated security=true;";
+                // conexion.ConnectionString = "server=ip\\SQLEXPRESS; database=CATALOGO_P3_DB;integrated security=true;";
+                // conexion.ConnectionString = "server=(local)\\SQLEXPRESS; database=CATALOGO_P3_DB;integrated security=true;";
+
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true;";
+                commando.CommandType = System.Data.CommandType.Text;
+                commando.CommandText = "SELECT a.Codigo, a.Nombre, a.Descripcion, a.Precio, m.Descripcion AS Marca, CASE WHEN c.Descripcion IS NULL THEN '0' ELSE c.Descripcion END AS Categoria, i.ImagenUrl FROM ARTICULOS a LEFT JOIN marcas m ON a.IdMarca = m.Id LEFT JOIN categorias c ON a.idcategoria = c.id LEFT JOIN IMAGENES i ON a.id = i.IdArticulo where a.Nombre like '%"+ buscar + "%' or a.Descripcion like '%" + buscar + "%' or m.Descripcion like '%" + buscar + "%' or c.Descripcion like '%" + buscar + "%';";
+                commando.Connection = conexion;
+                conexion.Open();
+                lector = commando.ExecuteReader();
+
+                while (lector.Read())
+                {
+
+                    Articulo aux = new Articulo();
+
+                    aux.ID = lector.GetString(0);
+                    aux.Nombre = lector.GetString(1);
+                    aux.Descripcion = lector.GetString(2);
+                    aux.Precio = lector.GetSqlMoney(3);
+                    aux.Marca = lector.GetString(4);
+                    aux.Categoria = lector.GetString(5);
+                    aux.Imagen = lector.GetString(6);
+
+                    /* aux.ID= (string)lector["Codigo"];
+                     aux.Nombre= (string)lector["Nombre"];
+                     aux.Descripcion= (string)lector["Descripcion"];
+                     aux.Precio= (SqlMoney) lector["Precio"];
+                     aux.Marca= (string)lector["Marca"];
+                     aux.Categoria= (string)lector["Categoria"];
+                     aux.Imagen = (string)lector["ImagenUrl"];*/
+
+
+
+                    Lista.Add(aux);
+
+
+                }
+
+
+
+                conexion.Close();
+                return Lista;
+            }
+
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            finally { conexion.Close(); }
+        }
+
+
     }
 }
